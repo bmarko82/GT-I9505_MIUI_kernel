9f856c28fb0832efcf7ea01f98996f307756d8a1
Joonsoo Kim <iamjoonsoo.kim@lge.com>
2013-04-05 03:16:14 +0100

    ARM: 7693/1: mm: clean-up in order to reduce to call kmap_high_get()
    In kmap_atomic(), kmap_high_get() is invoked for checking already
    mapped area. In __flush_dcache_page() and dma_cache_maint_page(),
    we explicitly call kmap_high_get() before kmap_atomic()
    when cache_is_vipt(), so kmap_high_get() can be invoked twice.
    This is useless operation, so remove one.
    
    v2: change cache_is_vipt() to cache_is_vipt_nonaliasing() in order to
    be self-documented
    
    Change-Id: I36e5a63fdae158d9e7562f564f7e593b351f6960
    Acked-by: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Git-commit: dd0f67f4747797f36f0c6bab7fed6a1f2448476d
    Git-repo: https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/
    [Few merge conflict resolved]
    Signed-off-by: Chintan Pandya <cpandya@codeaurora.org>
    
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>
    
    Conflicts:
    	arch/arm/mm/dma-mapping.c

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -815,44 +815,43 @@ static void dma_cache_maint_page(struct page *page, unsigned long offset,
 	size_t size, enum dma_data_direction dir,
 	void (*op)(const void *, size_t, int))
 {
-	unsigned long pfn;
-	size_t left = size;
-
-	pfn = page_to_pfn(page) + offset / PAGE_SIZE;
-	offset %= PAGE_SIZE;
-
 	/*
 	 * A single sg entry may refer to multiple physically contiguous
 	 * pages.  But we still need to process highmem pages individually.
 	 * If highmem is not configured then the bulk of this loop gets
 	 * optimized out.
 	 */
+	size_t left = size;
 	do {
 		size_t len = left;
 		void *vaddr;
 
-		page = pfn_to_page(pfn);
-
 		if (PageHighMem(page)) {
-			if (len + offset > PAGE_SIZE)
+			if (len + offset > PAGE_SIZE) {
+				if (offset >= PAGE_SIZE) {
+					page += offset / PAGE_SIZE;
+					offset %= PAGE_SIZE;
+				}
 				len = PAGE_SIZE - offset;
-			vaddr = kmap_high_get(page);
-			if (vaddr) {
-				vaddr += offset;
-				op(vaddr, len, dir);
-				kunmap_high(page);
-			} else if (cache_is_vipt()) {
-				/* unmapped pages might still be cached */
+			}
+
+			if (cache_is_vipt_nonaliasing()) {
 				vaddr = kmap_atomic(page);
 				op(vaddr + offset, len, dir);
 				kunmap_atomic(vaddr);
+			} else {
+				vaddr = kmap_high_get(page);
+				if (vaddr) {
+					op(vaddr + offset, len, dir);
+					kunmap_high(page);
+				}
 			}
 		} else {
 			vaddr = page_address(page) + offset;
 			op(vaddr, len, dir);
 		}
 		offset = 0;
-		pfn++;
+		page++;
 		left -= len;
 	} while (left);
 }
